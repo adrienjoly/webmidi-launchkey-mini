b5ab52bb199fbafffc3ef1036d511286dc007e56
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-12-29 17:11:41 +0100

    display state of track and play buttons

--- render.js
+++ render.js
@@ -1,27 +1,27 @@
 const halfKey = '<span class="nlk-half-key"></span>';
 
-const renderKey = ({ note, className }) => `<input class="nlk-key ${className || ''}" value="${note || ''}" />`;
+const renderKey = ({ note = '', className = '' }) => `<input class="nlk-key ${className}" value="${note}" />`;
 
-const renderNotes = ({ notes, values, className = '' }) => notes
-  .map(note => renderKey({ note: values[note], className }))
-  .join('\n');
-
-const renderBlackNotes = ({ notes, values }) => notes
-  .map(note => renderKey({ note: values[note], className: note ? 'nlk-black' : '' }))
+const renderNotes = ({ notes = [], values = {}, className = '' }) => notes
+  .map(note => renderKey({ note: values[note], className: note ? className : '' }))
   .join('\n');
 
 const renderWhiteNotes = ({ notes, values }) => renderNotes({ notes, values, className: 'nlk-white' });
 
+const renderBlackNotes = ({ notes, values }) => renderNotes({ notes, values, className: 'nlk-black' });
+
+const renderBinKey = value => renderKey({ note: value, className: 'nlk-white' });
+
 const renderHTML = state => ([
   '<h4>Pads</h4>',
-  renderWhiteNotes({
+  renderBinKey(state.trackPrev ? '<' : '') + halfKey + renderWhiteNotes({
     notes: [ 40, 41, 42, 43, 48, 49, 50, 51 ],
     values: state.padNotes
-  }),
-  renderWhiteNotes({
+  }) + halfKey + renderBinKey(state.playTop ? '▶' : ''),
+  renderBinKey(state.trackNext ? '>' : '') + halfKey + renderWhiteNotes({
     notes: [ 36, 37, 38, 39, 44, 45, 46, 47 ],
     values: state.padNotes
-  }),
+  }) + halfKey + renderBinKey(state.playBottom ? '▶' : ''),
   '<h4>Keys</h4>',
   halfKey + renderBlackNotes({
     notes: [ 49, 51, null, 54, 56, 58, null, 61, 63, null, 66, 68, 70 ],
