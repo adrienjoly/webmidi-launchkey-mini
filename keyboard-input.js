63fb790d6b5c1dccd64cf2540502622b36616402
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-12-30 19:32:07 +0100

    wip: keyboard-input generates fake MIDI events

--- keyboard-input.js
+++ keyboard-input.js
@@ -29,14 +29,62 @@ function playKeyTone(key) {
   }
 }
 
-window.addEventListener('keydown', function onkeydown(e){
-  playKeyTone(e.key);
-});
+function listenToKeyboardMessages(handler) {
 
-window.addEventListener('keyup', function onkeyup(e){
-  if (e.key === ',') {
-    switchPatch();
-  } else {
-    stopKeyTone(e.key);
+  function emit(message) {
+    setTimeout(handler.bind(null, message), 0);
   }
-});
+
+  const keyNotes = {
+    // pads
+    q: 36,
+    w: 37,
+    e: 38,
+    r: 39,
+    t: 44,
+    y: 45,
+    u: 46,
+    i: 47,
+    // white keys + black keys
+    z: 48, s: 49,
+    x: 50, d: 51,
+    c: 52,
+    v: 53, g: 54,
+    b: 55, h: 56,
+    n: 57, j: 58,
+    m: 59,
+  }
+
+  function getKeyNote(key) {
+    return keyNotes[key];
+  }
+
+  window.addEventListener('keydown', function onkeydown(e){
+    playKeyTone(e.key);
+    if (getKeyNote(e.key)) {
+      emit({
+        channel: 1,
+        command: 9,
+        note: getKeyNote(e.key),
+        velocity: 64,
+      });
+    }
+  });
+  
+  window.addEventListener('keyup', function onkeyup(e){
+    if (e.key === ',') {
+      switchPatch();
+    } else {
+      stopKeyTone(e.key);
+      if (getKeyNote(e.key)) {
+        emit({
+          channel: 1,
+          command: 8,
+          note: getKeyNote(e.key),
+          velocity: 0,
+        });
+      }
+    }
+  });
+    
+}
